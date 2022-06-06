f28c7aad84cd668104e794f27b8c8599cf66f826
gewazy <66756558+gewazy@users.noreply.github.com>
2022-06-06 12:25:22 +0200

    Add files via upload
    update file name

---
+++ init.py
@@ -0,0 +1,15 @@
+# pliki
+MDB = r"c:\PL-186_Zapalow_3D\01_database\Pl-186_Zapalow_3D.mdb"
+DPR = r"c:\PL-186_Zapalow_3D\08_raport_geodezyjny\PL_186_Raport_geodezyjny_DPR.xlsx"
+
+# zakresy linii punktów strzałowych i geofonów
+
+SOURCES_TRACK = "2064 And 2424"
+RECEIVERS_TRACK = "3001 And 3761"
+
+# w zależności za który dzień robimy raport, domyślnie ddiff = 0 jesli za diś
+DDIFF = 0
+
+# descriptory
+VIBRATORY_DSC = "'x40', 'x45', 'x30', 'x35', 'x20', 'x25', 'x10', 'x15', 'xm', 'xm5'"
+DYNAMITY_DSC = "'xt', 'xr'"
